3c6f46d40b773dfc148eeeebaa28a7e25c995eb3
Andy <nunuzuhh@gmail.com>
2021-12-01 01:15:13 -0800

    Added Pagination, Searching, View Single Post features.

--- src/api/index.js
+++ src/api/index.js
@@ -9,8 +9,12 @@ API.interceptors.request.use((req) => {
   return req;
 });
 
+// GET posts
+export const fetchPost = (id) => API.get(`/posts/${id}`);
+export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
+export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`);
+
 export const createPost = (newPost) => API.post('/posts', newPost);
-export const fetchPosts = () => API.get('/posts');
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
 export const likePost = (id) => API.patch(`/posts/${id}/like`);
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,32 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
-import FileBase from 'react-file-base64';
+import { useHistory } from 'react-router-dom';
+import ChipInput from 'material-ui-chip-input';
 
 import { createPost, updatePost } from '../../actions/posts';
 import useStyles from './styles';
 
 const Form = ({ currentId, setCurrentId }) => {
-  const [postData, setPostData] = useState({ title: '', message: '', tags: []});
-  const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
+  const [postData, setPostData] = useState({ title: '', message: '', tags: [] });
+  const post = useSelector((state) => (currentId ? state.posts.posts.find((message) => message._id === currentId) : null));
   const dispatch = useDispatch();
   const classes = useStyles();
   const user = JSON.parse(localStorage.getItem('profile'));
-
-  useEffect(() => {
-    if (post) setPostData(post);
-  }, [post]);
+  const history = useHistory();
 
   const clear = () => {
     setCurrentId(0);
     setPostData({ title: '', message: '', tags: []});
   };
 
+  useEffect(() => {
+    if (!post?.title) clear();
+    if (post) setPostData(post);
+  }, [post]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (currentId === 0) {
-      dispatch(createPost({ ...postData, username: user?.result?.username }));
+      dispatch(createPost({ ...postData, name: user?.result?.username }, history));
       clear();
     } else {
       dispatch(updatePost(currentId, { ...postData, username: user?.result?.username }));
@@ -38,19 +41,37 @@ const Form = ({ currentId, setCurrentId }) => {
     return (
       <Paper className={classes.paper}>
         <Typography variant="h6" align="center">
-          Please Sign In to create your own memories and like other's memories.
+          Please Sign In to create your own posts and like other's post.
         </Typography>
       </Paper>
     );
   }
 
+  const handleAdd = (tag) => {
+    setPostData({ ...postData, tags: [...postData.tags, tag] });
+  };
+
+  const handleDelete = (tagToDelete) => {
+    setPostData({ ...postData, tags: postData.tags.filter((tag) => tag !== tagToDelete) });
+  };
+
   return (
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-        <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Post'}</Typography>
+        <Typography variant="h6">{currentId ? `Editing "${post?.title}"` : 'Creating a Post'}</Typography>
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
         <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
-        <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
+        <div style={{ padding: '5px 0', width: '94%' }}>
+          <ChipInput
+            name="tags"
+            variant="outlined"
+            label="Tags"
+            fullWidth
+            value={postData.tags}
+            onAdd={(tag) => handleAdd(tag)}
+            onDelete={(tag) => handleDelete(tag)}
+          />
+        </div>
         <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
         <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
       </form>
